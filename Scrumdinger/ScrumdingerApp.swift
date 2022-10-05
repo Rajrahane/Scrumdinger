692ec948f317f5169d7ba51e4d231bfd946e36aa
Rajvaibhav Rahane <rajrahane10399@gmail.com>
2022-10-04 17:25:38 -0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Rajvaibhav Rahane on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
