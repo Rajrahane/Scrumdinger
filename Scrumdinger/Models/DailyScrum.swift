cca6274a06a3e70f41b2bc5e6f5200963adce8b0
Rajvaibhav Rahane <rajrahane10399@gmail.com>
2022-10-05 23:32:27 -0700

    setup ScrumsView as root

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -7,18 +7,27 @@
 
 import Foundation
 
-struct DailyScrum {
+struct DailyScrum: Identifiable {
+    let id: UUID
     var title: String
     var attendees: [String]
     var lengthInMinutes: Int
     var theme: Theme
+    
+    init(id: UUID=UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
 }
 
 extension DailyScrum {
     static let sampleData: [DailyScrum] =
     [
         DailyScrum(title: "Design", attendees: ["Cathy","Daisy","Simon","Jonathan"], lengthInMinutes: 10, theme: .yellow),
-        DailyScrum(title: "App Dev", attendees: ["Rutuja","Raj","Milind", "Pritesh"], lengthInMinutes: 15, theme: .buttercup),
-        DailyScrum(title: "Web Dev", attendees: ["Rutuja","Harsharani","Vidisha","Nutan"], lengthInMinutes: 20, theme: .lavender)
+        DailyScrum(title: "App Dev", attendees: ["Rutuja","Raj","Milind", "Pritesh"], lengthInMinutes: 15, theme: .orange),
+        DailyScrum(title: "Web Dev", attendees: ["Rutuja","Harsharani","Vidisha","Nutan"], lengthInMinutes: 20, theme: .poppy)
     ]
 }
--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -5,4 +5,23 @@
 //  Created by Rajvaibhav Rahane on 10/5/22.
 //
 
-import Foundation
+import SwiftUI
+
+struct ScrumsView: View{
+    let scrums: [DailyScrum]
+    
+    var body: some View{
+        List{
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider{
+    static var previews: some View{
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
